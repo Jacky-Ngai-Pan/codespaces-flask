fb8db5b60d16388457a96060988949e084d6171d
Jacky-Ngai-Pan <88673898+Jacky-Ngai-Pan@users.noreply.github.com>
2024-02-06 14:20:34 +0000

    update

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html", title="Hello")
+@app.route('/')
+def home():
+    response = requests.get('https://official-joke-api.appspot.com/random_joke')
+    joke = response.json()
+    return render_template('index.html', joke=joke)
+
+if __name__ == '__main__':
+    app.run(debug=True)
